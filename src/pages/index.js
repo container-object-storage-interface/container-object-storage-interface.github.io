9ae32660afe5d9c44da032383d09977e4a2971a5
Sidhartha Mani <sidhartha_mani@apple.com>
2023-11-02 12:58:08 -0700

    add official COSI drivers

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,68 +9,86 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Object Storage Vendors',
-    imageUrl: 'img/vendors.svg',
+    title: 'Azure COSI Driver',
+    repoURL: 'https://github.com/Azure/azure-cosi-driver',
     description: (
       <>
-Vendors can write their own COSI drivers to enable applications to use their object storage system. Learn how to write a COSI driver <Link to="docs/write-driver/introduction">here</Link>.
+        Official COSI driver for Azure Blob Store.
       </>
     ),
   },
   {
-    title: 'Application Developers',
-    imageUrl: 'img/app_developers.svg',
+    title: 'Ceph COSI Driver',
+    repoURL: 'https://github.com/ceph/ceph-cosi',
     description: (
       <>
-Make your application COSI compatible and seamlessly switch between object storage providers without any application code changes. Learn more it <Link to="docs/user-guide">here</Link>.
+        Official COSI driver for Ceph Rados Gateway (RGW).
+      </>
+    ),
+  },
+  {
+    title: 'Dell COSI Driver',
+    repoURL: 'https://github.com/dell/cosi',
+    description: (
+      <>
+        Official COSI driver for Dell ObjectScale.
+      </>
+    ),
+  },
+  {
+    title: 'SUSE COSI Driver',
+    repoURL: 'https://github.com/aquarist-labs/s3gw-cosi-driver',
+    description: (
+      <>
+        Official COSI driver for SUSE s3gw.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
+function Feature({ repoURL, title, description }) {
+  /*const imgUrl = useBaseUrl(imageUrl);*/
   return (
     <div className={clsx('col col--6', styles.feature)}>
-      {imgUrl && (
+      {/* {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
-      )}
+      )} */}
       <div className="text--center">
-        <h3>{title}</h3>
+        <Link to={repoURL}><h3>{title}</h3> </Link>
         <p>{description}</p>
       </div>
     </div>
   );
 }
 
-function User({name, imageUrl, organizationURL, organizationName, organizationLogo , github, twitter, pinned}) {
+function User({ name, imageUrl, organizationURL, organizationName, organizationLogo, github, twitter, pinned }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className="leaderboard" key={name} style={{"margin": "auto", "alignItems": "center", "justifyContents": "center", "display": "flex", "flexDirection": "row"}}>
+    <div className="leaderboard" key={name} style={{ "margin": "auto", "alignItems": "center", "justifyContents": "center", "display": "flex", "flexDirection": "row" }}>
 
       <a href={imageUrl} key={name}>
-        <img src={imageUrl} alt={name} title={name} style={{"height": "100px", "width": "100px", "marginRight": "1px", "marginBottom": "0px", "padding": "50px"}}/>
+        <img src={imageUrl} alt={name} title={name} style={{ "height": "100px", "width": "100px", "marginRight": "1px", "marginBottom": "0px", "padding": "50px" }} />
       </a>
 
-      <div className="leaderboard-details" style={{"textAlign": "left"}}>
+      <div className="leaderboard-details" style={{ "textAlign": "left" }}>
 
         <strong>{name}</strong><br />
 
-        <div style={{"display": "flex", "flexDirection": "row", "alignItems": "center", "fontWeight": "700", "fontColor": "black"}}>
-          <img src={organizationLogo} alt='org' style={{"height": "24px", "width": "24px", "marginRight": "5px", "marginBottom": "5px", "padding": "0"}} />
-          <a className="github" href={organizationURL} style={{"color": "black"}}>{organizationName}</a>
+        <div style={{ "display": "flex", "flexDirection": "row", "alignItems": "center", "fontWeight": "700", "fontColor": "black" }}>
+          <img src={organizationLogo} alt='org' style={{ "height": "24px", "width": "24px", "marginRight": "5px", "marginBottom": "5px", "padding": "0" }} />
+          <a className="github" href={organizationURL} style={{ "color": "black" }}>{organizationName}</a>
         </div>
 
-        <div style={{"display": "flex", "flexDirection": "row", "alignItems": "center", "fontWeight": "700", "fontColor": "black"}}>
-          <img src='/img/github.svg' alt='github' style={{"height": "24px", "width": "24px", "marginRight": "5px", "marginBottom": "5px", "padding": "0"}} />
-          <a className="github" href={"https://github.com/" + github} style={{"color": "black"}}>{github}</a>
+        <div style={{ "display": "flex", "flexDirection": "row", "alignItems": "center", "fontWeight": "700", "fontColor": "black" }}>
+          <img src='/img/github.svg' alt='github' style={{ "height": "24px", "width": "24px", "marginRight": "5px", "marginBottom": "5px", "padding": "0" }} />
+          <a className="github" href={"https://github.com/" + github} style={{ "color": "black" }}>{github}</a>
         </div>
 
-        <div style={{"display": "flex", "flexDirection": "row", "alignItems": "center", "fontWeight": "700", "fontColor": "black"}}>
-          <img src='/img/twitter.png' alt='twitter' style={{"height": "20px", "width": "24px", "marginRight": "5px", "marginBottom": "5px", "padding": "0"}} />
-          <a className="twitter" href={"https://twitter.com/" + twitter} style={{"color": "black"}}>{twitter}</a>
+        <div style={{ "display": "flex", "flexDirection": "row", "alignItems": "center", "fontWeight": "700", "fontColor": "black" }}>
+          <img src='/img/twitter.png' alt='twitter' style={{ "height": "20px", "width": "24px", "marginRight": "5px", "marginBottom": "5px", "padding": "0" }} />
+          <a className="twitter" href={"https://twitter.com/" + twitter} style={{ "color": "black" }}>{twitter}</a>
         </div>
 
       </div>
@@ -81,7 +99,7 @@ function User({name, imageUrl, organizationURL, organizationName, organizationLo
 
 export default function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`${siteConfig.title}`}
